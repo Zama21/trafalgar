8c7273b61a0ec312e2ded7db4d9f795bd0aec175
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-07-10 01:32:16 +0500

    Почти сделал верстку, немного подправить нужно и можно добавлять адаптив. Вынес константы, решил так же вынести компоненте CardPrice

---
+++ src/components/CardPrice/Card.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import styled from 'styled-components';
+import Typography from '../../Typography.tsx';
+import Button from '../Button.tsx';
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  border: 1px solid;
+  border-color: ${({ theme }) => theme.colors.coolGray20};
+  border-radius: 8px;
+  padding: 48px 24px;
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const CardTitle = styled.div`
+  ${({ theme }) => theme.typography.h3}
+`;
+
+const Price = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+  p {
+    font-size: ${({ theme }) => theme.typography.bodyS};
+  }
+`;
+
+const PriceOld = styled.div`
+  text-decoration: line-through;
+  color: ${({ theme }) => theme.colors.coolGray40};
+  font-size: ${({ theme }) => theme.typography.h1};
+`;
+
+const PriceNew = styled.div`
+  font-size: ${({ theme }) => theme.typography.h1};
+`;
+
+const ListContainer = styled.div`
+  ${({ theme }) => theme.typography.bodyL};
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const CardPrice = ({ title, description, oldPrice, newPrice, itemList }) => (
+  <Card>
+    <CardHeader>
+      <CardTitle>{title}</CardTitle>
+      <Typography variant="bodyL">{description}</Typography>
+    </CardHeader>
+    <Price>
+      <PriceOld>{oldPrice}</PriceOld>
+      <PriceNew>{newPrice}</PriceNew>
+      <p>{newPrice} USD каждый месяц</p>
+    </Price>
+    <Button width="135px" height="56px" primary="primary">
+      Начать
+    </Button>
+    <ListContainer>
+      {itemList &&
+        itemList.map((item, index) => (
+          <ListItem key={index}>
+            <img src="/assets/check.svg" alt="check" />
+            {item}
+          </ListItem>
+        ))}
+    </ListContainer>
+  </Card>
+);
--- src/pages/Home/SectionPrice.tsx
+++ src/pages/Home/SectionPrice.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { CardPrice } from '../../components/CardPrice/Card';
+import { items } from '../../constants/PriceConstant';
 
 const MainContainer = styled.main`
   max-width: 100vw;
@@ -37,7 +39,79 @@ const HeaderTwo = styled.div`
 const HeaderText = styled.div`
   ${({ theme }) => theme.typography.bodyL}
 `;
+
+const MainContainerInfo = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Discount = styled.div`
+  background: #f0f0f0;
+  text-align: center;
+  border-radius: 12px;
+  width: 96px;
+  height: 20px;
+  padding: 4px 8px;
+  position: absolute;
+  top: -30px;
+  ${({ theme }) => theme.typography.bodyXS}
+`;
+
+const TimeSwitch = styled.div`
+  width: 149px;
+  height: 25px;
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+  align-items: center;
+  margin: 0 auto;
+  ${({ theme }) => theme.typography.bodyL}
+`;
+
+const TimeOption = styled.div`
+  cursor: pointer;
+`;
+
+const Switch = styled.div`
+  position: relative;
+  width: 32px;
+  height: 16px;
+  background: #458ff6;
+  border-radius: 33px;
+  padding: 2px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+const SwitchToggle = styled.div`
+  transition: 300ms all;
+  content: '';
+  position: absolute;
+  width: 12px;
+  height: 12px;
+  border-radius: 35px;
+  top: 50%;
+  left: ${(props) => (props.checked ? '16px' : '4px')};
+  background: white;
+  transform: translate(0, -50%);
+`;
+
+const ContainerCard = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 24px;
+`;
+
 const SectionPrice = () => {
+  const [checked, setChecked] = useState(false);
+
+  const toggleSwitch = () => {
+    setChecked(!checked);
+  };
+
   return (
     <MainContainer>
       <Container>
@@ -53,7 +127,39 @@ const SectionPrice = () => {
             </HeaderText>
           </HeaderInfo>
         </ContainerHeader>
-        <TimeSwitch></TimeSwitch>
+        <MainContainerInfo>
+          <Discount>20% дешевле</Discount>
+          <TimeSwitch>
+            <TimeOption onClick={() => setChecked(false)}>Год</TimeOption>
+            <Switch onClick={toggleSwitch}>
+              <SwitchToggle checked={checked} />
+            </Switch>
+            <TimeOption onClick={() => setChecked(true)}>Месяц</TimeOption>
+          </TimeSwitch>
+        </MainContainerInfo>
+        <ContainerCard>
+          <CardPrice
+            title="Стажер"
+            description="Для начинающих специалистов"
+            oldPrice="$35"
+            newPrice="$25"
+            itemList={items.slice(0, 5)}
+          />
+          <CardPrice
+            title="Специалист"
+            description="Для специалистов среднего уровня"
+            oldPrice="$50"
+            newPrice="$40"
+            itemList={items.slice(0, 8)}
+          />
+          <CardPrice
+            title="Эксперт"
+            description="Для опытных специалистов"
+            oldPrice="$70"
+            newPrice="$60"
+            itemList={items.slice(0, 12)}
+          />
+        </ContainerCard>
       </Container>
     </MainContainer>
   );
