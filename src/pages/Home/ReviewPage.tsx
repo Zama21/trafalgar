f4c726c4b1ce54fde7e4f9b78f6f61927b9cb168
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-07-10 20:49:51 +0500

    feat карточка с каруселью (ReviwPage)

--- src/pages/Home/ReviewPage.tsx
+++ src/pages/Home/ReviewPage.tsx
@@ -3,9 +3,9 @@ import Slider from 'react-slick';
 import styled from 'styled-components';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Typography from '../../../Typography.tsx';
-import { cards } from '../../../constants/cards.ts';
-import { NextArrow, PrevArrow } from './ArrowSlider.tsx';
+import Typography from '../../Typography.tsx';
+import { cards } from '../../constants/cards.ts';
+import { NextArrow, PrevArrow } from './Slider/ArrowSlider.tsx';
 
 const settings = {
   dots: false,
