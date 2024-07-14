6a752c6b1c16ac2a6b22692db0a46b860bdde434
Sistinelove <142403330+Sistinelove@users.noreply.github.com>
2024-07-14 14:58:10 +0500

    wip

--- src/constants/ArtikleConstants..ts
+++ src/constants/ArtikleConstants..ts
@@ -16,7 +16,21 @@ export const articles: Article[] = [
     text: 'Информация для пациентов. Мигрень. Симптомы, диагностика, лечение, профилактика',
   },
   {
-    img: '/assets/ArticleImg3.svg',
+    img:[
+      '/assets/Article/01.png',
+      '/assets/Article/02.png',
+      '/assets/Article/03.png',
+      '/assets/Article/04.png',
+      '/assets/Article/05.png',
+      '/assets/Article/06.png',
+      '/assets/Article/07.png',
+      '/assets/Article/08.png',
+      '/assets/Article/09.png',
+      '/assets/Article/10.png',
+      '/assets/Article/11.png',
+
+    ]
+    img: '/public/picture.png',
     title: 'Доказательная медицина',
     text: 'Читая статьи, и получая консультации на нашем сайте, вы часто будете встречаться...',
   },
